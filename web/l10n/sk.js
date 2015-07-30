ccd430c8392a697418af0f7b1e361d7cd0456301
JurajBaran <juraj.baran@outlook.com>
2015-07-30 09:14:23 +0200

    Create sk.js

---
+++ web/l10n/sk.js
@@ -0,0 +1,61 @@
+var strings = {
+    shared_loading: 'Načítava...',
+    shared_save: 'Ulož',
+    shared_cancel: 'Zruš',
+    shared_add: 'Pridať',
+    shared_edit: 'Upraviť',
+    shared_remove: 'Odstrániť',
+    shared_remove_confirm: 'Odstrániť položku?',
+
+    error_title: 'Chyba',
+    error_unknown: 'Neznáma chyba',
+
+    user_name: 'Meno',
+    user_email: 'E-mail',
+    user_password: 'Heslo',
+    user_admin: 'Admin',
+
+    login_title: 'Login',
+    login_register: 'Registrovať',
+    login_login: 'Login',
+    login_failed: 'Nesprávna e-mailová adresa alebo heslo',
+    login_created: 'Nový užívateľ sa zaregistroval',
+    login_logout: 'Odhlásiť',
+
+    device_dialog: 'Zariadenie',
+    device_title: 'Zariadenia',
+    device_name: 'Meno',
+    device_identifier: 'Identifikátor',
+
+    settings_title: 'Nastavenia',
+    settings_user: 'Účet',
+    settings_server: 'Server',
+    settings_users: 'Užívateľ ia',
+
+    report_title: 'Správy',
+    report_device: 'Zariadenie',
+    report_from: 'Od',
+    report_to: 'do',
+    report_show: 'Zobraziť',
+    report_clear: 'Vyčistiť',
+
+    position_time: 'Čas',
+    position_valid: 'Platné',
+    position_latitude: 'Šírka',
+    position_longitude: 'Dĺžka',
+    position_altitude: 'Nadmorská výška',
+    position_speed: 'Rýchlosť jazdy',
+    position_course: 'Kurz',
+    position_address: 'Adresa',
+    position_protocol: 'Protokol',
+
+    server_title: 'Nastavenie servera',
+    server_zoom: 'Zoom',
+    server_registration: 'Registrácia',
+
+    map_title: 'Mapa',
+
+    state_title: 'Štát',
+    state_name: 'Parameter',
+    state_value: 'Hodnota'
+};
